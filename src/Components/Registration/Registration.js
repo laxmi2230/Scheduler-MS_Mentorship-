ca3496504b8b50267159074e0309c153680706e7
laxmi <laxmimenon18@gmail.com>
2021-11-13 20:55:15 +0530

    working

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,39 +1,126 @@
-import React from 'react'
+import React, {Component} from 'react';
+import axios from 'axios';
+//import { Button } from 'react-bootstrap';
 import './Registration.css';
 
-const Registration = () => {
+class Registration extends Component {
+  constructor(){
+    super()
+    this.state = {
+      fullname:'',
+      dob:'',
+      offline_online:'',
+      subjectcode:'',
+      subjectname:''
+    }
+    this.changeFullname=this.changeFullname.bind(this)
+    this.changeDob=this.changeDob.bind(this)
+    this.changeOfflineonline=this.changeOfflineonline.bind(this)
+    this.changeSubjectcode=this.changeSubjectcode.bind(this)
+    this.changeSubjectname=this.changeSubjectname.bind(this)
+    this.onSubmit=this.onSubmit.bind(this)
+    this.onChange=this.onChange.bind(this)
+    
+  }
+
+  changeFullname(event){
+    this.setState({
+      fullname:event.target.value
+    })
+  }
+  changeDob(event){
+    this.setState({
+      dob:event.target.value
+    })
+  }
+  changeOfflineonline(event){
+    this.setState({
+      offline_online:event.target.value
+    })
+  }
+  changeSubjectcode(event){
+    this.setState({
+      subjectcode:event.target.value
+    })
+  }
+  changeSubjectname(event){
+    this.setState({
+      subjectname:event.target.value
+    })
+  }
+  onSubmit(event){
+    event.preventDefault()
+
+    const registered = {
+      fullname: this.state.fullname,
+      dob: this.state.dob,
+      offline_online: this.state.offline_online,
+      subjectcode: this.state.subjectcode,
+      subjectname: this.state.subjectname
+    }
+    axios.post('http://localhost:3001/api/usermodel1', registered)
+        .then(response => console.log(response.data))
+    
+      this.setState({
+      fullname:'',
+      dob:'',
+      offline_online:'',
+      subjectcode:'',
+      subjectname:''
+    })
+ }
+  onChange(){
+    //console.log("xyz");
+    const blogs = {
+      offline_online : this.state.offline_online
+    }
+    axios.post('http://localhost:3001/api/usermodel2', blogs)
+  .then(response=>console.log(response.data));
+  }
+  render() {
     return (
-        <div>
-        <article class="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
-        <main class="pa4 black-80">
-        <form class="measure center">
-          <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-            <legend class="f4 fw6 ph0 mh0">Registration</legend>
-            <div class="mt3">
-              <label class="db fw6 lh-copy f6" for="full-name">Full Name</label>
-              <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="name" name="name"  id="name" />
-            </div>
-            <div class="mv3">
-              <label class="db fw6 lh-copy f6" for="date-of-birth">Date-of-Birth</label>
-              <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="dob" name="number"  id="number" />
-            </div>
-            <div class="mv3">
-              <label class="db fw6 lh-copy f6" for="subject-code">Subject Code</label>
-              <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="subjectcode" name="subjectcode"  id="subjectcode" />
-            </div>
-            <div class="mv3">
-              <label class="db fw6 lh-copy f6" for="subject-name">Subject Name</label>
-              <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="subjectname" name="subjectname"  id="subjectname" />
-            </div>
-          </fieldset>
-          <div class="">
-            <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Submit" />
+      <div>
+      <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
+      <main className="pa4 black-80">
+      <form className="measure center" onSubmit={this.onSubmit}>
+        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+          <legend className="f4 fw6 ph0 mh0">Registration</legend>
+          <div className="mt3">
+            <label className="db fw6 lh-copy f6" htmlFor="full-name">Full Name</label>
+            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="name" placeholder="FullName" onChange={this.changeFullname} value={this.state.fullname} />
           </div>
-        </form>
-      </main>
-     </article>
-    </div> 
-    )
+          <div className="mv3">
+            <label className="db fw6 lh-copy f6" htmlFor="date-of-birth">Date-of-Birth</label>
+            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="number" placeholder="DOB" onChange={this.changeDob} value={this.state.dob}  />
+          </div>
+          <div className="mv3">
+            <label className="db fw6 lh-copy f6" htmlFor="subject-code">Subject Code</label>
+            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="subjectcode" placeholder="subjectcode(Only Char)" onChange={this.changeSubjectcode} value={this.state.subjectcode} />
+            &nbsp;&nbsp;&nbsp;
+            <br/>
+            <button variant="dark" onClick={this.onChange}>
+             Find Avaliable Seats
+            </button>
+          </div>
+          <div className="mv3">
+            <label className="db fw6 lh-copy f6" htmlFor="offline/online">Offline/Online</label>
+            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="offline_online" placeholder="offline/online" onChange={this.changeOfflineonline} value={this.state.Offline_online} />
+          </div>
+          <div className="mv3">
+            <label className="db fw6 lh-copy f6" htmlFor="subject-name">Subject Name</label>
+            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="subjectname" placeholder="subjectname" onChange={this.changeSubjectname} value={this.state.subjectname}  />
+          </div>
+        </fieldset>
+        <div className="">
+          <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Submit" />
+        </div>
+      </form>
+    </main>
+   </article>
+  </div> 
+  )
+  }
+   
 }
 
-export default Registration
+export default Registration;
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,44 +1,103 @@
-import React from 'react';
-
+import React, { Component} from 'react';
+import axios from 'axios';
 import './Signup.css'
 
 
-const Signup = () => {
+class Signup extends Component {
+  constructor(){
+    super()
+    this.state = {
+      email:'',
+      password:'',
+      subjectcode:'',
+      subjectname:''
+    }
+    this.changeEmail=this.changeEmail.bind(this)
+    this.changePassword=this.changePassword.bind(this)
+    this.changeSubjectcode=this.changeSubjectcode.bind(this)
+    this.changeSubjectname=this.changeSubjectname.bind(this)
+    this.onSubmit=this.onSubmit.bind(this)
+  }
+
+  changeEmail(event){
+    this.setState({
+      email:event.target.value
+    })
+  }
+  changePassword(event){
+    this.setState({
+      password:event.target.value
+    })
+  }
+  changeSubjectcode(event){
+    this.setState({
+      subjectcode:event.target.value
+    })
+  }
+  changeSubjectname(event){
+    this.setState({
+      subjectname:event.target.value
+    })
+  }
+  onSubmit(event){
+    event.preventDefault()
+
+    const registered = {
+      email: this.state.email,
+      password: this.state.password,
+      subjectcode: this.state.subjectcode,
+      subjectname: this.state.subjectname
+    }
+    axios.post('http://localhost:3001/api/usermodel', registered)
+        .then(response => console.log(response.data))
+    
+      this.setState({
+      email:'',
+      password:'',
+      subjectcode:'',
+      subjectname:''
+    })
+    
+  }
+  
+
+  render() {
     return (
-        <div>
-        <article class="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
-        <main class="pa4 black-80">
-        <form class="measure center">
-          <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-            <legend class="f4 fw6 ph0 mh0">Sign In</legend>
-            <div class="mt3">
-              <label class="db fw6 lh-copy f6" for="email-address">Email</label>
-              <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
-            </div>
-            <div class="mv3">
-              <label class="db fw6 lh-copy f6" for="password">Password</label>
-              <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
-            </div>
-            <div class="mv3">
-              <label class="db fw6 lh-copy f6" for="subject-code">Subject Code</label>
-              <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="subjectcode" name="subjectcode"  id="subjectcode" />
-            </div>
-            <div class="mv3">
-              <label class="db fw6 lh-copy f6" for="subject-name">Subject Name</label>
-              <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="subjectname" name="subjectname"  id="subjectname" />
-            </div>
-          </fieldset>
-          <div class="">
-            <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+      <div className="container">
+      <article className="dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
+      <main className="pa4 black-80">
+      <form className="measure center" onSubmit={this.onSubmit}>
+        <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
+          <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+          <div class="mt3">
+            <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" placeholder="email" onChange={this.changeEmail} value={this.state.email}   />
           </div>
-          <div class="lh-copy mt3">
-            <a href="#0" class="f6 link dim black db">Sign up</a>
+          <div className="mv3">
+            <label className="db fw6 lh-copy f6" for="password">Password</label>
+            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" placeholder="password" onChange={this.changePassword} value={this.state.password}   />
           </div>
-        </form>
-      </main>
-     </article>
-    </div> 
-    )
+          <div className="mv3">
+            <label className="db fw6 lh-copy f6" for="subject-code">Subject Code</label>
+            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="subjectcode" placeholder="subjectcode(Only Char)" onChange={this.changeSubjectcode} value={this.state.subjectcode}  />
+          </div>
+          <div className="mv3">
+            <label className="db fw6 lh-copy f6" for="subject-name">Subject Name</label>
+            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="subjectname" placeholder="subjectname" onChange={this.changeSubjectname} value={this.state.subjectname}  />
+          </div>
+        </fieldset>
+        <div className="">
+          <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+        </div>
+        <div className="lh-copy mt3">
+          <a href="#0" className="f6 link dim black db">Back</a>
+        </div>
+      </form>
+    </main>
+   </article>
+  </div> 
+  );
+  }
 }
 
-export default Signup
+export default Signup;
