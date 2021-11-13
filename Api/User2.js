f5b2e48a044429c7e8649bdcc288338af521746d
laxmi <laxmimenon18@gmail.com>
2021-11-13 21:48:35 +0530

    working

--- Api/User2.js
+++ Api/User2.js
@@ -13,23 +13,27 @@ route.post('/', async (req, res)=> {
     user2.subjectcode = subjectcode;
     let usermodel2 = new User2(user2);
     await usermodel2.save();
-    res.json (usermodel2);
+    res.json (usermodel2); 
 })
 
-route.post("/subjectcode", (req,res,next)=>{
-    console.log("abc")
-    {/*try {
-        const resp = await user2.find(
+route.post("/seats", async (req,res,next)=>{
+  
+   // console.log("abc") 
+   var subjectcode=req.body.subjectcode;  
+    try {
+       // let user2 = {};
+       
+        const resp = await User2.find(
           {subjectcode:subjectcode}
         );
         res.send(resp);
       } catch (err) {
-        res.status(500);
+        res.status(500);  
         res.send({
           err: "Server error",
         });
         next(err);
-      }*/}
+      } 
 })
 
 module.exports = route;
